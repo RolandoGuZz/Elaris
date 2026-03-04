17f5aa6aef946b40f697c5b8d2f32c6d188daf5b
RolandoGuZz <guzmancorreonova@gmail.com>
2026-03-04 11:16:40 -0600

    feat: update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,1 +1,17 @@
-// docuemtno de exportaciond de las colecciones
+import { defineCollection, z } from "astro:content";
+
+const career = defineCollection({
+  schema: z.object({
+    name: z.string(),
+    duration: z.string(),
+    description: z.string(),
+    programDirection: z.string(),
+    mission: z.string(),
+    vision: z.string(),
+    objective: z.string(),
+    admissionProfile: z.string(),
+    graduateProfile: z.string(),
+    professionalField: z.string(),
+    studyPlan: z.string(),
+  }),
+});
